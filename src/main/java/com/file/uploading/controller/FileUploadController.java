91ba5417724a16dbb049a70cf2ae5dca1f94012e
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-13 12:22:08 +0500

    This code is not working in my pc.However, it works there

--- src/main/java/com/file/uploading/controller/FileUploadController.java
+++ src/main/java/com/file/uploading/controller/FileUploadController.java
@@ -2,13 +2,22 @@ package com.file.uploading.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class FileUploadController {
 
     @PostMapping("/upload-file")
-    public ResponseEntity<String> UploadFile(){
+    public ResponseEntity<String> UploadFile(@RequestParam("file") MultipartFile file){
+
+        System.out.println(file.getOriginalFilename());
+        System.out.println(file.getSize());
+        System.out.println(file.getContentType());
+        System.out.println(file.getName());
+
+
         return ResponseEntity.ok("working");
 
     }
